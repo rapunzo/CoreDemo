f19d5ae64ed24c93149cd692edba34d51a33b19c
Mansur Dinçer <mansurdincer@outlook.com>
2022-02-20 22:40:16 +0300

    Add project files.

---
+++ EntityLayer/Concrete/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public string? Description { get; set; }
+        public bool Status { get; set; }
+
+    }
+}
